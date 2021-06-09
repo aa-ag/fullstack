bb73f1ca20236a8bf989fd3098f04a9aea3d57cf
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-09 15:20:01 -0500

    added @property serialize() function to be able to send JSON objects in serialized format

--- dbsetup.py
+++ dbsetup.py
@@ -25,11 +25,23 @@ class Menu(Base):
     ___tablename__ = "menu_item"
     ############------------ MAPPER ------------#######
     name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     course = Column(String(250))
     description = Column(String(250))
     price = Column(String(8))
-    restaurant_id = Column(Integer, ForeignKey('restaurant_id'))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+
+        }
 
 
 ############------------ CONFIGURATION 2/2 ------------############
