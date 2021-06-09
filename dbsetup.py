44277a7cae7d03a969bc6e9132378ed93b870f0c
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-09 15:17:02 -0500

    added class Restaurant() in dbsetup.py

--- dbsetup.py
+++ dbsetup.py
@@ -1,22 +1,25 @@
-############------------ CONFIGURATION ------------############
+############------------ CONFIGURATION 1/2 ------------############
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from sqlalchemy.sql.expression import null
 
 
-Base =  declarative_base()
-###### insert at end of file ######
+Base = declarative_base()
 
+
+############------------ CLASS ------------############
+class Restaurant(Base):
+    ############------------ TABLE INFO -------########
+    __tablename__ = "restaurant"
+    ############------------ MAPPER ------------#######
+    name = Column(String(80))
+    id = Column(Integer, primary_key=True)
+
+
+############------------ CONFIGURATION 2/2 ------------############
 engine = create_engine('sqlite://restaurantmenu.db')
 
 Base.metadata.create_all(engine)
-
-############------------ GLOBAL VARIABLES ------------############
-
-############------------ FUNCTIONS ------------############
-
-############------------ DRIVER CODE ------------############
-if __name__ == "__main__":
-    pass
