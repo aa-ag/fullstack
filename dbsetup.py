ef40b2c8779f38104684837ca34729671640e2ed
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-09 15:29:21 -0500

    fixed typos/errors in dbsetup.py -- now running/creating db

--- dbsetup.py
+++ dbsetup.py
@@ -1,11 +1,10 @@
 ############------------ CONFIGURATION 1/2 ------------############
+import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-from sqlalchemy.sql.expression import null
-
 
 Base = declarative_base()
 
@@ -13,22 +12,22 @@ Base = declarative_base()
 ############------------ CLASS ------------############
 class Restaurant(Base):
     ############------------ TABLE INFO -------########
-    __tablename__ = "restaurant"
+    __tablename__ = 'restaurant'
     ############------------ MAPPER ------------#######
-    name = Column(String(80))
     id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
 
 
 ############------------ CLASS ------------############
 class Menu(Base):
     ############------------ TABLE INFO -------########
-    ___tablename__ = "menu_item"
+    __tablename__ = 'menu_item'
     ############------------ MAPPER ------------#######
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
-    course = Column(String(250))
     description = Column(String(250))
     price = Column(String(8))
+    course = Column(String(250))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
 
@@ -49,6 +48,6 @@ class Menu(Base):
 
 
 ############------------ CONFIGURATION 2/2 ------------############
-engine = create_engine('sqlite://restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenu.db')
 
 Base.metadata.create_all(engine)
