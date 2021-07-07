a86f22badf0f9d58d581e30c8a47cf10ce4d386c
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-06 20:30:37 -0500

    added logic to set up webserver to listen to port 8080 and let user know if it's running or if ^c has been enetered and web server is closed

--- webserver.py
+++ webserver.py
@@ -1,2 +1,19 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+
+class webserverHandler(BaseHTTPRequestHandler):
+    pass
+
+
+def main():
+    try:
+        port = 8080
+        server = HTTPServer(('', port), webserverHandler)
+        print("Web server running on port %s" % port)
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("^C entered, stopping web server")
+        server.socket.close()
+
+if __name__ == "__main__":
+    pass
